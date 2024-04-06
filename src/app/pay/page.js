e8f9ee913904c04b19c319d289e1f0d1b6d8816f
Thompson Manda <thompsonmanda08@outlook.com>
2024-04-06 11:37:37 +0200

    add error handling

--- src/app/pay/page.js
+++ src/app/pay/page.js
@@ -1,9 +1,27 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+import { Input, Button, Spinner } from "@nextui-org/react";
 
 function Pay() {
+  const [isLoading, setIsLoading] = useState(false);
+  function handlePayment() {}
   return (
     <div className="w-full h-screen grid place-items-center">
-      <div className="p-5 border border-slate-400/20 rounded-lg w-96"></div>
+      <div className="p-5 border border-slate-400/20 rounded-lg w-96 flex flex-col gap-5 justify-center items-center">
+        <p className="text-center font-semibold">
+          Software Development Course <br />K 300/month
+        </p>
+
+        <Button
+          onClick={handlePayment}
+          disabled={isLoading}
+          color={"primary"}
+          className="max-w-xs font-semibold text-white"
+        >
+          {isLoading ? <Spinner size="sm" color="white" /> : "Pay Now"}
+        </Button>
+      </div>
     </div>
   );
 }
